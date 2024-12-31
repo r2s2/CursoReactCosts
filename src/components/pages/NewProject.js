3fed3048fc933ef1948c2dc70d28bcc26bbc7816
r2s2 <rodrigo.sacramento@gmail.com>
2024-12-31 13:58:19 -0300

    Começo 3o dia

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,7 +1,15 @@
-function NewProject() {
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProject.module.css'
+
+
+function NewProject () {
     return (
-        <h1>New Project</h1>
+     <div className={styles.newproject_container}>
+    <h1>Criar Projeto</h1>
+    <p> Crie seu projeto para depois adicionar os serviços</p>
+    <ProjectForm />
+    </div>   
     )
 }
 
-export default NewProject
+export default NewProject;
