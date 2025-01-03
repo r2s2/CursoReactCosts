58fe91c428ce387ef53bba7813a71cf9f2d401a2
r2s2 <rodrigo.sacramento@gmail.com>
2025-01-03 20:13:06 -0300

    4º dia completo

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,9 @@
 import { useLocation } from 'react-router-dom'
 import Message from '../layout/Message'
+import styles from './Projects.module.css'
+import LinkButton from '../layout/LinkButton'
+
+import Container from '../layout/Container'
 
 function Projects() {
     const location = useLocation()
@@ -9,11 +13,20 @@ function Projects() {
         message = location.state.message
     }
     return (
-       <div>
+       <div className={styles.project_container}>
+        <div className={styles.title_container}>
         <h1>
             Meus projetos
+            
         </h1>
+        <LinkButton to="/newproject" text="Criar projeto" />
+        </div>
+        
        {message &&  <Message type="success" msg={message} />}
+       <Container customClass="start">
+        <p>Projetos...</p>
+       </Container>
+
 
        </div>
     )
