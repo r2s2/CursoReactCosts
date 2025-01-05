35cc964d84e8413e47f770ee40ced94517dca7f0
r2s2 <rodrigo.sacramento@gmail.com>
2025-01-05 16:07:11 -0300

    Adiciona novo projeto ao db.json e implementa exibição de projetos na página

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,17 +1,39 @@
 import { useLocation } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import Message from '../layout/Message'
 import styles from './Projects.module.css'
 import LinkButton from '../layout/LinkButton'
+import ProjectCard from '../project/ProjectCard'
 
 import Container from '../layout/Container'
 
 function Projects() {
+
+    const [projects, setProjects] = useState([])
+
     const location = useLocation()
     let message = ''
 
     if(location.state) {
         message = location.state.message
     }
+
+    useEffect(() => {
+       fetch('http://localhost:5000/projects', {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+        }, 
+       }).then(resp => resp.json())
+       .then((data) => {
+        console.log(data)
+        setProjects(data)
+
+       })
+       .catch(err => console.log(err))
+    },[])
+
+
     return (
        <div className={styles.project_container}>
         <div className={styles.title_container}>
@@ -24,7 +46,16 @@ function Projects() {
         
        {message &&  <Message type="success" msg={message} />}
        <Container customClass="start">
-        <p>Projetos...</p>
+        {projects.length > 0 &&
+        projects.map((project)=> (
+            <ProjectCard 
+            name={project.name}
+            id={project.id}
+            budget={project.budget}
+            //category={project.category.name} 
+            key={project.id}
+            />
+        ))}
        </Container>
 
 
