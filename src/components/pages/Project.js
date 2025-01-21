533b8be9343c2576c06ceeb5da9c1d203b299a01
r2s2 <rodrigo.sacramento@gmail.com>
2025-01-20 21:32:22 -0300

    Adiciona funcionalidade para exibir e adicionar serviços no projeto

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -12,6 +12,7 @@ function Project() {
     const { id } = useParams()
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
     const [type, setType] = useState()
 
@@ -30,6 +31,7 @@ function Project() {
 },  [id])
 
 function editPost(project){
+    setMessage('')
     // budget validation
     if (project.budget < project.cost){
         setMessage('O orçamento não poder ser menor do que o custo do projeto')
@@ -58,6 +60,11 @@ function editPost(project){
 
 function toggleProjectForm() {
     setShowProjectForm(!showProjectForm)
+
+}
+
+function toggleServiceForm() {
+    setShowServiceForm(!showServiceForm)
 
 }
 
@@ -91,6 +98,25 @@ function toggleProjectForm() {
 
                 )}
                 </div>    
+                <div className={styles.service_form_container}>
+                    <h2>
+                        Adicione um serviço:
+                    </h2>
+                    <button className={styles.btn} onClick={toggleServiceForm}>
+                        {!showServiceForm ? "Adicionar serviço" : "Fechar"}
+
+                    </button>
+                    <div className={styles.project_info}>
+                        {showServiceForm && <div> Formulário do serviço</div>}
+
+                    </div>
+
+                </div>
+                <h2>Serviços</h2>
+            </Container>
+            <Container customClass="start">
+                <p>Itens de serviços</p>
+
             </Container>
         </div>
     ): (
