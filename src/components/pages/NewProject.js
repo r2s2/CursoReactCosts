6b47136ab0e57dc8e3a7f62b602a82613786f688
r2s2 <rodrigo.sacramento@gmail.com>
2025-02-03 21:36:05 -0300

    Adiciona novo projeto com ID e orçamento, e atualiza a navegação após criação do projeto

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -6,7 +6,7 @@ import styles from './NewProject.module.css'
 
 function NewProject () {
 
-    const history = useNavigate() 
+    const navigate = useNavigate() 
 
     function createPost (project){
         // inicializar
@@ -23,7 +23,7 @@ function NewProject () {
         .then((data) => {
             console.log(data)
             //redirect
-            history('/projects', {message : 'Projeto criado com sucesso!'})
+            navigate('/projects', {message : 'Projeto criado com sucesso!'})
         }
     )
         .catch((err) => console.log(err))
